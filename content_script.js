aa3d12b303811871963daf7eb2da3c94a6679cb1
Chris Read <24418909+rcreadii@users.noreply.github.com>
2021-05-11 21:04:01 -0400

    work on reddit where the page doesnt reload in between links

--- content_script.js
+++ content_script.js
@@ -8,30 +8,46 @@ if (!tabTime) {
 }
 
 // for clicking link on site, staying in the tab
-const lastUrl = document.referrer;
-if (lastUrl !== "") {
-  const lastUrlNew = new URL(lastUrl);
+let tabUrl = window.sessionStorage.getItem("tabUrl");
+
+if (!tabUrl) {
+  console.log("tabUrl if");
+  tabUrl = document.URL;
+  window.sessionStorage.setItem("tabUrl", tabUrl);
+}
+
+const currentUrl = document.URL;
+
+console.log("currentUrl", currentUrl);
+
+if (tabUrl !== currentUrl) {
+  console.log(tabUrl, currentUrl);
+  const lastUrlNew = new URL(tabUrl);
   console.log(lastUrlNew);
   let lastUrlPathname = "";
   if (lastUrlNew) lastUrlPathname = lastUrlNew.pathname;
   const currentUrlPathname = window.location.pathname;
+  console.log(lastUrlPathname, currentUrlPathname);
 
-  if (
-    lastUrlPathname !== currentUrlPathname &&
-    window.performance.getEntriesByType("navigation")[0]["type"] !== "reload"
-  ) {
+  if (lastUrlPathname !== currentUrlPathname) {
     console.log("second if");
     window.sessionStorage.removeItem("tabTime");
+    window.sessionStorage.removeItem("tabUrl");
     const time = new Date();
     window.sessionStorage.setItem("tabTime", time);
+    window.sessionStorage.setItem("tabUrl", currentUrl);
   }
 }
 
+console.log("there");
 setInterval(() => {
+  console.log("here");
   let title = document.querySelector("title");
   const currentTime = new Date();
   const originalTime = Date.parse(window.sessionStorage.getItem("tabTime"));
   const timeDiff = Math.floor((currentTime - originalTime) / 1000);
+
+  console.log(timeDiff, timeDiff);
 
   let currentTitle = title.innerText;
   let currentTitleArr = currentTitle.split(" ");
@@ -45,5 +61,3 @@ setInterval(() => {
   }
   title.innerText = `【${timeDiff}s】 ${currentTitle}`;
 }, 100);
-
-// replace document.referrer with saving it ourselves
